0d10735995a84742d06f206fefe2429504383c7f
Kuntadhutade <kuntadhutade@gmail.com>
2025-11-08 15:40:48 +0530

    add authlogin folder inside src/pages

---
+++ src/pages/AuthLogin/AuthLogin.jsx
@@ -0,0 +1,79 @@
+
+
+
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+function AuthLogin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:3000/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        alert(" Login successful!");
+        localStorage.setItem("token", data.token);
+        navigate("/dashboard");
+      } else {
+        alert(data.message || "Invalid credentials");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Server error. Please try again.");
+    }
+  };
+
+  return (
+    <div className="container d-flex align-items-center justify-content-center vh-100">
+      <div className="card shadow p-4" style={{ maxWidth: "400px", width: "100%" }}>
+        <h3 className="text-center mb-4 text-primary fw-bold">Login</h3>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label>Email</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <button className="btn btn-primary w-100 mt-2" type="submit">
+            Login
+          </button>
+        </form>
+
+        <p className="text-center mt-3">
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AuthLogin;
